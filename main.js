03715556817116c7c57f0b78cb06d80eed53f58c
Hagaradel <68480407+Hagaradel@users.noreply.github.com>
2024-11-11 00:10:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var swiper=new Swiper(".home",{
+    spaceBetween:30,
+    centerSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
